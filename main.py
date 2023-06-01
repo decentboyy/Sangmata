2358fbb0e90a45ef771abbd27d95f317affbfc7d
Decent Boy <88884010+decentboyy@users.noreply.github.com>
2023-06-02 03:23:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import logging
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, Filters, MessageHandler
+import telebot
 
 # Set up logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -11,15 +10,20 @@ logger = logging.getLogger(__name__)
 tracked_users = {}
 
 
-def start(update: Update, _) -> None:
+bot = telebot.TeleBot("1719065252:AAH0y8WEkXrVdvH1ShG51PkC2SZEtLXVU40")
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
     """Handler for the /start command."""
-    update.message.reply_text("Hello! I will track name and username changes for you. "
-                              "Add me to a group and give me admin rights to start tracking.")
+    bot.reply_to(message, "Hello! I will track name and username changes for you. "
+                          "Add me to a group and give me admin rights to start tracking.")
 
 
-def track(update: Update, _) -> None:
+@bot.message_handler(commands=['track'])
+def track(message):
     """Handler for tracking name and username changes."""
-    user = update.message.from_user
+    user = message.from_user
 
     if user.id in tracked_users:
         tracked_users[user.id]['name'] = user.full_name
@@ -30,27 +34,28 @@ def track(update: Update, _) -> None:
             'username': user.username
         }
 
-    update.message.reply_text("You have been added to the tracking list.")
+    bot.reply_to(message, "You have been added to the tracking list.")
 
 
-def notify_name_change(update: Update, _) -> None:
-    """Handler for name changes."""
-    user = update.message.from_user
-    user_id = user.id
+@bot.message_handler(content_types=['new_chat_members'])
+def new_chat_member(message):
+    """Handler for new chat members."""
+    user = message.new_chat_member
 
-    if user_id in tracked_users and user.full_name != tracked_users[user_id]['name']:
+    if user.id in tracked_users and user.full_name != tracked_users[user.id]['name']:
         text = f"🔔 Name Change Alert! 🔔\n\n"
-        text += f"User: {user.full_name} ({user_id})\n"
-        text += f"Old Name: {tracked_users[user_id]['name']}\n"
+        text += f"User: {user.full_name} ({user.id})\n"
+        text += f"Old Name: {tracked_users[user.id]['name']}\n"
         text += f"New Name: {user.full_name}"
 
-        update.message.reply_text(text)
-        tracked_users[user_id]['name'] = user.full_name
+        bot.reply_to(message, text)
+        tracked_users[user.id]['name'] = user.full_name
 
 
-def notify_username_change(update: Update, _) -> None:
+@bot.message_handler(func=lambda message: message.entities and any(entity.type in ['mention', 'text_mention'] for entity in message.entities))
+def username_change(message):
     """Handler for username changes."""
-    user = update.message.from_user
+    user = message.from_user
     user_id = user.id
 
     if user_id in tracked_users and user.username != tracked_users[user_id]['username']:
@@ -59,36 +64,21 @@ def notify_username_change(update: Update, _) -> None:
         text += f"Old Username: {tracked_users[user_id]['username']}\n"
         text += f"New Username: {user.username}"
 
-        update.message.reply_text(text)
+        bot.reply_to(message, text)
         tracked_users[user_id]['username'] = user.username
 
 
-def error(update: Update, context) -> None:
-    """Log errors."""
-    logger.warning(f"Update {update} caused error {context.error}")
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    """Handler for other message types."""
+    bot.reply_to(message, "I'm sorry, I only track name and username changes.")
 
 
-def main() -> None:
-    """Start the bot."""
-    # Set up the Telegram Bot
-    updater = Updater("1719065252:AAH0y8WEkXrVdvH1ShG51PkC2SZEtLXVU40")
-
-    # Get the dispatcher to register handlers
-    dispatcher = updater.dispatcher
-
-    # Register handlers
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("track", track))
-    dispatcher.add_handler(MessageHandler(Filters.entity("bold") | Filters.entity("italic"), notify_name_change))
-    dispatcher.add_handler(MessageHandler(Filters.username, notify_username_change))
-    dispatcher.add_error_handler(error)
-
-    # Start the bot
-    updater.start_polling()
-
-    # Run the bot until you press Ctrl-C
-    updater.idle()
+@bot.message_handler(func=lambda message: True)
+def error_handler(message):
+    """Handler for errors."""
+    logger.warning(f"Update {message} caused an error.")
 
 
-if __name__ == '__main__':
-    main()
+# Start the bot
+bot.polling()
